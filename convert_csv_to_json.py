3a302e761f7acb236e73e88c063c1ff7c2fccabb
trieulv <trieulv@bap.jp>
2019-03-05 09:28:34 +0700

    [Server-Framework] load json data

--- convert_csv_to_json.py
+++ convert_csv_to_json.py
@@ -5,19 +5,25 @@ import os
 
 def convert_csv_to_jason(data_path):
     json_file = open(os.path.join(data_path, 'json_file.txt'), 'w+')
+    fieldnames = ['linkProduct',
+                  'note',
+                  'address',
+                  'paid',
+                  'costSite',
+                  'orderBy']
     with open(os.path.join(data_path, 'sheet1.csv'), 'r') as data:
         next(data, None)
         reader = csv.DictReader(data,
-                                fieldnames=['linkProduct',
-                                            'note',
-                                            'address',
-                                            'paid',
-                                            'costSite',
-                                            'orderBy'])
+                                fieldnames=fieldnames)
         rows = list(reader)
         json_file.write('[\n')
         for idx, row in enumerate(rows):
-            if idx != len(rows)-1:
+            for fieldname in fieldnames:
+                row[fieldname] = row[fieldname].\
+                    replace('\"\"', '').\
+                    replace('\"', '').\
+                    replace('\n', ', ')
+            if idx != len(rows) - 1:
                 json.dump(row, json_file)
                 json_file.write(',\n')
             else:
@@ -28,6 +34,11 @@ def convert_csv_to_jason(data_path):
 def main():
     convert_csv_to_jason(data_path='./data/')
     print('csv is converted!')
+    print('='*80)
+    with open('./data/json_file.txt') as f:
+        data = json.load(f)
+    print(data)
+    return data
 
 
 if __name__ == "__main__":
