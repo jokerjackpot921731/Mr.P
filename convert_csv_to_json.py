4d9e8eb134acce634b836ef670e5fc20611c85e1
trieulv <trieulv@bap.jp>
2019-03-10 12:02:52 +0700

    [Server-Framework] Fix columns of csv file, change using columns in feature_columns/custom_column_utils.py

--- convert_csv_to_json.py
+++ convert_csv_to_json.py
@@ -5,18 +5,23 @@ import pandas as pd
 
 from feature_columns.custom_column_utils import _USE_CSV_COLUMNS
 
-def preprocessing_csv(data_path):
-  data_df = pd.read_csv(data_path, usecols=_USE_CSV_COLUMNS)
-  data_df.to_csv(data_path, index=False, sep=',', columns=_USE_CSV_COLUMNS)
+def preprocessing_csv(raw_data_path, cleaned_data_path):
+  data_df = pd.read_csv(raw_data_path, usecols=_USE_CSV_COLUMNS)
+  data_df.to_csv(cleaned_data_path, index=False, sep=',', columns=_USE_CSV_COLUMNS)
 
 def convert_csv_to_json(data_path):
-    json_file = open(os.path.join(data_path, 'json_file.json'), 'w+')
-    csv_path = os.path.join(data_path, 'items-Sheet1.csv')
+    json_path = os.path.join(data_path, 'json/json_file.json')
+    raw_data_path = os.path.join(data_path, 'items-Sheet1.csv')
+    cleaned_data_path = os.path.join(data_path, 'processed/cleaned_sheet.csv')
+
+    json_file = open(json_path, 'w+')
     fieldnames = _USE_CSV_COLUMNS
+
     # processing data csv
-    preprocessing_csv(csv_path)
+    preprocessing_csv(raw_data_path, cleaned_data_path)
+
     # convert to json
-    with open(csv_path, 'r') as data:
+    with open(cleaned_data_path, 'r') as data:
         next(data, None)
         reader = csv.DictReader(data, fieldnames=fieldnames)
         rows = list(reader)
@@ -25,7 +30,6 @@ def convert_csv_to_json(data_path):
         json_file.write('[\n')
         for idx, row in enumerate(rows):
             for fieldname in fieldnames:
-                print(fieldname)
                 row[fieldname] = row[fieldname].\
                     replace('\"\"', '').\
                     replace('\"', '').\
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -3,7 +3,7 @@ import { User } from "../models/schema/User";
 import { hashPassword } from "../utils/encryption";
 import { getID } from "../utils/commonFunctions";
 import {PythonShell} from "python-shell";
-import fs from "fs";
+import {fs} from "fs";
 
 class UserService {
   static async boot () {
@@ -45,10 +45,11 @@ class UserService {
   }
   static async getCSV () {
     try {
-      PythonShell.run('convert_csv_to_json.py', null, function (err) {
+      PythonShell.run('convert_csv_to_json.py', null, function (err, results) {
         if (err) throw err;
         console.log('finished');
-        var contents = fs.readFileSync('./data/json_file.json');
+        console.log(results)
+        var contents = fs.readFileSync('./data/json/json_file.json');
         var jsonContents = JSON.parse(contents);
         console.log(jsonContents);
       });
