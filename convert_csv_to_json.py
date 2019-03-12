e4e0106208a131bb835ed412e106a63a1e574d12
trieulv <trieulv@bap.jp>
2019-03-12 17:40:51 +0700

    [Server-Framework] modify convert_csv_to_json.py

--- convert_csv_to_json.py
+++ convert_csv_to_json.py
@@ -24,35 +24,41 @@ def convert_csv_to_json(data_path):
     fieldnames = _USE_CSV_COLUMNS
 
     # processing data csv
-    preprocessing_csv(raw_data_path, cleaned_data_path)
+    try:
+        preprocessing_csv(raw_data_path, cleaned_data_path)
+    except Exception as e:
+        print("There are no raw data", e)
+        return None
 
     # convert to json
-    with open(raw_data_path, 'r') as data:
-        next(data, None)
-        reader = csv.DictReader(data, fieldnames=fieldnames)
-        rows = list(reader)
+    try:
+        with open(cleaned_data_path, 'r') as data:
+            next(data, None)
+            reader = csv.DictReader(data, fieldnames=fieldnames)
+            rows = list(reader)
 
-        # write file .txt
-        json_file.write('[\n')
-        for idx, row in enumerate(rows):
-            for fieldname in fieldnames:
-                row[fieldname] = row[fieldname].\
-                    replace('\"\"', '').\
-                    replace('\"', '').\
-                    replace('\n', ', ')
-            if idx != len(rows) - 1:
-                json.dump(row, json_file)
-                json_file.write(',\n')
-            else:
-                json.dump(row, json_file)
-                json_file.write('\n')
-        json_file.write(']')
-
+            # write file .txt
+            json_file.write('[\n')
+            for idx, row in enumerate(rows):
+                for fieldname in fieldnames:
+                    row[fieldname] = row[fieldname].\
+                        replace('\"\"', '').\
+                        replace('\"', '').\
+                        replace('\n', ', ')
+                if idx != len(rows) - 1:
+                    json.dump(row, json_file)
+                    json_file.write(',\n')
+                else:
+                    json.dump(row, json_file)
+                    json_file.write('\n')
+            json_file.write(']')
+    except Exception as e:
+        print("Could not convert file", e)
+        return None
+    print("Json file is already.")
 
 def main():
     convert_csv_to_json(data_path='./data/')
-    print("Json file is already.")
-
 
 if __name__ == "__main__":
     main()
---
+++ dictionary.js
@@ -0,0 +1,3 @@
+var dictionary = {false: '', true: 'two'};
+var a = 5
+console.log(dictionary[0 > a] || dictionary[a > 0]);
